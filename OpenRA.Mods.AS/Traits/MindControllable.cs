a9be973135efe7cfd3b88ae1493ed6ff88ce2620
Zimmermann Gyula <graiondilach@hotmail.com>
2019-06-16 21:04:39 +0200

    Expose the owner to yaml if a mindcontrolled actor is freed after it's original owner lost the game.
    And change the default value to Creeps from Neutral.

--- OpenRA.Mods.AS/Traits/MindControllable.cs
+++ OpenRA.Mods.AS/Traits/MindControllable.cs
@@ -25,6 +25,9 @@ namespace OpenRA.Mods.AS.Traits
 		[Desc("The sound played when the mindcontrol is revoked.")]
 		public readonly string[] RevokeControlSounds = { };
 
+		[Desc("Map player to transfer this actor to if the owner lost the game.")]
+		public readonly string FallbackOwner = "Creeps";
+
 		public override object Create(ActorInitializer init) { return new MindControllable(init.Self, this); }
 	}
 
@@ -104,7 +107,7 @@ namespace OpenRA.Mods.AS.Traits
 			if (creatorOwner.WinState == WinState.Lost)
 				self.ChangeOwner(self.World.WorldActor.Owner);
 			else
-				self.ChangeOwner(creatorOwner);
+				self.ChangeOwner(self.World.Players.First(p => p.InternalName == info.FallbackOwner));
 
 			UnlinkMaster(self, master);
 
