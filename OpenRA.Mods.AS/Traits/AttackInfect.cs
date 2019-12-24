89ed58afb5c21a5c102fff01608f57f84723e589
Zimmermann Gyula <graiondilach@hotmail.com>
2019-12-24 16:18:57 +0100

    Implement parasite logic.
    Loosely based upon RV's with AttackLeap mixed in here'n'there. And with bugfixes. Bunch of bugfixes.

---
+++ OpenRA.Mods.AS/Traits/AttackInfect.cs
@@ -0,0 +1,101 @@
+﻿#region Copyright & License Information
+/*
+ * Copyright 2015- OpenRA.Mods.AS Developers (see AUTHORS)
+ * This file is a part of a third-party plugin for OpenRA, which is
+ * free software. It is made available to you under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation. For more information, see COPYING.
+ */
+#endregion
+
+using OpenRA.Activities;
+using OpenRA.Mods.AS.Activities;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Primitives;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.AS.Traits
+{
+	[Desc("Move onto the target then execute the attack.")]
+	public class AttackInfectInfo : AttackFrontalInfo, Requires<MobileInfo>
+	{
+		[Desc("Range of the final joust of the infector.")]
+		public readonly WDist JoustRange = WDist.Zero;
+
+		[Desc("Conditions that last from start of the joust until the attack.")]
+		[GrantedConditionReference]
+		public readonly string JoustCondition = "jousting";
+
+		[FieldLoader.Require]
+		[Desc("How much damage to deal.")]
+		public readonly int Damage;
+
+		[FieldLoader.Require]
+		[Desc("How often to deal the damage.")]
+		public readonly int DamageInterval;
+
+		[Desc("Damage types for the infection damage.")]
+		public readonly BitSet<DamageType> DamageTypes = default(BitSet<DamageType>);
+
+		[Desc("If an external actor delivers more damage than this value, the infector is killed immediately.",
+			"Use -1 to never kill the infector.")]
+		public readonly int SuppressionDamageThreshold = -1;
+
+		[Desc("If the infected actor receives more damage from external sources than this value, the infector dies along with the infected.",
+			"Use -1 to never kill the infector.")]
+		public readonly int SuppressionSumThreshold = -1;
+
+		[Desc("Damage types which allows the infector survive when it's host dies.")]
+		public readonly BitSet<DamageType> SurviveHostDamageTypes = default(BitSet<DamageType>);
+
+		public override object Create(ActorInitializer init) { return new AttackInfect(init.Self, this); }
+	}
+
+	public class AttackInfect : AttackFrontal
+	{
+		readonly AttackInfectInfo info;
+
+		ConditionManager conditionManager;
+		int joustToken = ConditionManager.InvalidConditionToken;
+
+		public AttackInfect(Actor self, AttackInfectInfo info)
+			: base(self, info)
+		{
+			this.info = info;
+		}
+
+		protected override void Created(Actor self)
+		{
+			conditionManager = self.TraitOrDefault<ConditionManager>();
+			base.Created(self);
+		}
+
+		protected override bool CanAttack(Actor self, Target target)
+		{
+			if (target.Type != TargetType.Actor)
+				return false;
+
+			if (self.Location == target.Actor.Location && HasAnyValidWeapons(target))
+				return true;
+
+			return base.CanAttack(self, target);
+		}
+
+		public void GrantJoustCondition(Actor self)
+		{
+			if (conditionManager != null && !string.IsNullOrEmpty(info.JoustCondition))
+				joustToken = conditionManager.GrantCondition(self, info.JoustCondition);
+		}
+
+		public void RevokeJoustCondition(Actor self)
+		{
+			if (joustToken != ConditionManager.InvalidConditionToken)
+				joustToken = conditionManager.RevokeCondition(self, joustToken);
+		}
+
+		public override Activity GetAttackActivity(Actor self, Target newTarget, bool allowMove, bool forceAttack, Color? targetLineColor)
+		{
+			return new Infect(self, newTarget, this, info, targetLineColor);
+		}
+	}
+}
---
+++ OpenRA.Mods.AS/Traits/Infectable.cs
@@ -0,0 +1,216 @@
+#region Copyright & License Information
+/*
+ * Copyright 2015- OpenRA.Mods.AS Developers (see AUTHORS)
+ * This file is a part of a third-party plugin for OpenRA, which is
+ * free software. It is made available to you under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation. For more information, see COPYING.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using OpenRA.Mods.Common;
+using OpenRA.Mods.Common.Activities;
+using OpenRA.Mods.Common.Traits;
+using OpenRA.Primitives;
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.AS.Traits
+{
+	[Desc("Handle infection by infector units.")]
+	public class InfectableInfo : ConditionalTraitInfo, Requires<HealthInfo>
+	{
+		[Desc("Damage types that removes the infector.")]
+		public readonly BitSet<DamageType> RemoveInfectorDamageTypes = default(BitSet<DamageType>);
+
+		[Desc("Damage types that kills the infector.")]
+		public readonly BitSet<DamageType> KillInfectorDamageTypes = default(BitSet<DamageType>);
+
+		[GrantedConditionReference]
+		[Desc("The condition to grant to self while infected by any actor.")]
+		public readonly string InfectedCondition = null;
+
+		[GrantedConditionReference]
+		[Desc("Condition granted when being infected by another actor.")]
+		public readonly string BeingInfectedCondition = null;
+
+		[Desc("Conditions to grant when infected by specified actors.",
+			"A dictionary of [actor id]: [condition].")]
+		public readonly Dictionary<string, string> InfectedByConditions = new Dictionary<string, string>();
+
+		[GrantedConditionReference]
+		public IEnumerable<string> LinterConditions { get { return InfectedByConditions.Values; } }
+
+		public override object Create(ActorInitializer init) { return new Infectable(init.Self, this); }
+	}
+
+	public class Infectable : ConditionalTrait<InfectableInfo>, ISync, ITick, INotifyCreated, INotifyDamage, INotifyKilled, IRemoveInfector
+	{
+		readonly Health health;
+
+		public Tuple<Actor, AttackInfect, AttackInfectInfo> Infector;
+		public int[] FirepowerMultipliers = new int[] { };
+
+		[Sync]
+		public int Ticks;
+
+		ConditionManager conditionManager;
+		int beingInfectedToken = ConditionManager.InvalidConditionToken;
+		Actor enteringInfector;
+		int infectedToken = ConditionManager.InvalidConditionToken;
+		int infectedByToken = ConditionManager.InvalidConditionToken;
+
+		int dealtDamage = 0;
+
+		public Infectable(Actor self, InfectableInfo info)
+			: base(info)
+		{
+			health = self.Trait<Health>();
+		}
+
+		protected override void Created(Actor self)
+		{
+			conditionManager = self.TraitOrDefault<ConditionManager>();
+
+			base.Created(self);
+		}
+
+		public bool TryStartInfecting(Actor self, Actor infector)
+		{
+			if (infector != null)
+			{
+				if (enteringInfector == null)
+				{
+					enteringInfector = infector;
+
+					if (conditionManager != null)
+					{
+						if (beingInfectedToken == ConditionManager.InvalidConditionToken && !string.IsNullOrEmpty(Info.BeingInfectedCondition))
+							beingInfectedToken = conditionManager.GrantCondition(self, Info.BeingInfectedCondition);
+					}
+
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		public void GrantCondition(Actor self)
+		{
+			if (conditionManager != null)
+			{
+				if (infectedToken == ConditionManager.InvalidConditionToken && !string.IsNullOrEmpty(Info.InfectedCondition))
+					infectedToken = conditionManager.GrantCondition(self, Info.InfectedCondition);
+
+				string infectedByCondition;
+				if (Info.InfectedByConditions.TryGetValue(Infector.Item1.Info.Name, out infectedByCondition))
+					infectedByToken = conditionManager.GrantCondition(self, infectedByCondition);
+			}
+		}
+
+		public void RevokeCondition(Actor self, Actor infector = null)
+		{
+			if (conditionManager != null)
+			{
+				if (infector != null)
+				{
+					if (enteringInfector == infector)
+					{
+						enteringInfector = null;
+
+						if (beingInfectedToken != ConditionManager.InvalidConditionToken)
+							beingInfectedToken = conditionManager.RevokeCondition(self, beingInfectedToken);
+					}
+				}
+				else
+				{
+					if (infectedToken != ConditionManager.InvalidConditionToken)
+						infectedToken = conditionManager.RevokeCondition(self, infectedToken);
+
+					if (infectedByToken != ConditionManager.InvalidConditionToken)
+						infectedByToken = conditionManager.RevokeCondition(self, infectedByToken);
+				}
+			}
+		}
+
+		void RemoveInfector(Actor self, bool kill, AttackInfo e)
+		{
+			if (Infector != null && !Infector.Item1.IsDead)
+			{
+				Infector.Item1.TraitOrDefault<IPositionable>().SetPosition(Infector.Item1, self.CenterPosition);
+				self.World.AddFrameEndTask(w =>
+				{
+					w.Add(Infector.Item1);
+
+					if (kill)
+					{
+						if (e != null)
+							Infector.Item1.Kill(e.Attacker, e.Damage.DamageTypes);
+						else
+							Infector.Item1.Kill(self);
+					}
+					else
+						Infector.Item1.QueueActivity(false, new Move(Infector.Item1, self.Location));
+
+					RevokeCondition(self);
+					Infector = null;
+					FirepowerMultipliers = new int[] { };
+					dealtDamage = 0;
+				});
+			}
+		}
+
+		void INotifyDamage.Damaged(Actor self, AttackInfo e)
+		{
+			if (Infector != null)
+			{
+				if (e.Attacker != Infector.Item1)
+				{
+					if (Infector.Item3.SuppressionDamageThreshold > 0 && e.Damage.Value > Infector.Item3.SuppressionDamageThreshold)
+					{
+						RemoveInfector(self, true, e);
+						return;
+					}
+
+					dealtDamage += e.Damage.Value;
+				}
+
+				if (e.Damage.DamageTypes.Overlaps(Info.KillInfectorDamageTypes))
+					RemoveInfector(self, true, e);
+				else if (e.Damage.DamageTypes.Overlaps(Info.RemoveInfectorDamageTypes))
+					RemoveInfector(self, false, e);
+			}
+		}
+
+		void INotifyKilled.Killed(Actor self, AttackInfo e)
+        {
+            if (Infector != null)
+            {
+				var kill = Infector.Item3.SuppressionSumThreshold > 0 && dealtDamage > Infector.Item3.SuppressionSumThreshold;
+				kill |= !Infector.Item3.SurviveHostDamageTypes.Overlaps(e.Damage.DamageTypes);
+				RemoveInfector(self, kill, e);
+            }
+		}
+
+		void ITick.Tick(Actor self)
+		{
+			if (!IsTraitDisabled && Infector != null)
+			{
+				if (--Ticks < 0)
+				{
+					var damage = Util.ApplyPercentageModifiers(Infector.Item3.Damage, FirepowerMultipliers);
+					health.InflictDamage(self, Infector.Item1, new Damage(damage, Infector.Item3.DamageTypes), false);
+
+					Ticks = Infector.Item3.DamageInterval;
+				}
+			}
+		}
+
+		void IRemoveInfector.RemoveInfector(Actor self, bool kill, AttackInfo e)
+		{
+			RemoveInfector(self, kill, e);
+		}
+	}
+}
