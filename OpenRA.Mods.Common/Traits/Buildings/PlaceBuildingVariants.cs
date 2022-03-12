61c68a6eea99a6344fdd819b98a9160d76e60005
dnqbob <dnqbob@outlook.com>
2022-03-12 20:20:49 +0800

    Upstream upgrade for AI variant building

--- OpenRA.Mods.Common/Traits/Buildings/PlaceBuildingVariants.cs
+++ OpenRA.Mods.Common/Traits/Buildings/PlaceBuildingVariants.cs
@@ -21,7 +21,7 @@ namespace OpenRA.Mods.Common.Traits
 		[Desc("Variant actors that can be cycled between when placing a structure.")]
 		public readonly string[] Actors = null;
 
-		[Desc("Facings reference of variant actors. Should always have the length of number of Variant Actors + 1, due to the first element refer to actor itself")]
+		[Desc("Facing of the non-variant actor, followed by facings for each variant actor. The length equals the length of Actors + 1.")]
 		public readonly WAngle[] Facings = null;
 
 		public override object Create(ActorInitializer init) { return new PlaceBuildingVariants(); }
