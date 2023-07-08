afe3b3bb8ca05cd6628445b0b2a46e8248fc809d
Mustafa Alperen Seki <mustafaoyunda37@gmail.com>
2023-07-08 17:24:43 +0300

    ProductionQueueFromSelection now considers AlwaysVisibleForFaction too.

--- OpenRA.Mods.Common/Traits/ProductionQueueFromSelection.cs
+++ OpenRA.Mods.Common/Traits/ProductionQueueFromSelection.cs
@@ -63,7 +63,7 @@ namespace OpenRA.Mods.Common.Traits
 					.FirstOrDefault(q => q.Enabled && types.Contains(q.Info.Type));
 			}
 
-			if (queue == null || !queue.BuildableItems().Any())
+			if (queue == null || (!queue.BuildableItems().Any() && !queue.AlwaysVisible))
 				return;
 
 			if (tabsWidget.Value != null)
